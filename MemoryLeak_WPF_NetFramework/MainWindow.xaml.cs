3df015faaed25b50f6dfd5883d260fef97c08952
Tomáš Filip <tomas.filip@meac.cz>
2022-02-09 08:01:51 +0100

    NetFramework 4.8

---
+++ MemoryLeak_WPF_NetFramework/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MemoryLeak_WPF_NetFramework
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<TestModel> Models = new ObservableCollection<TestModel>();
+        private bool doIt = true;
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Main_DataGrid.ItemsSource = Models;
+            // this.Main_ItemsControl.ItemsSource = Models;
+
+            Task.Run(StartCycle);
+            Task.Run(BreakCycle);
+        }
+
+        private async Task BreakCycle()
+        {
+            await Task.Delay(15000);
+            doIt = false;
+        }
+
+        private async Task StartCycle()
+        {
+            while (doIt)
+            {
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    foreach (var model in Enumerable.Range(0, 500).Select(_ => new TestModel()).ToList())
+                    {
+                        Models.Add(model);
+                    }
+                });
+                await Task.Delay(200);
+            }
+            Application.Current.Dispatcher.Invoke(() => Models.Clear());
+        }
+    }
+}
---
+++ MemoryLeak_WPF_NetFramework/TestModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryLeak_WPF_NetFramework
+{
+    public class TestModel : INotifyPropertyChanged
+    {
+        public TestModel()
+        {
+            Id = Guid.NewGuid();
+            TestProp1 = "TestProp1";
+            TestProp2 = "TestProp2";
+            TestProp3 = "TestProp3";
+            TestProp4 = "TestProp4";
+            TestProp5 = "TestProp5";
+            TestProp6 = "TestProp6";
+        }
+
+        public Guid Id { get; set; }
+
+        public string TestProp1 { get; set; }
+
+        public string TestProp2 { get; set; }
+
+        public string TestProp3 { get; set; }
+
+        public string TestProp4 { get; set; }
+
+        public string TestProp5 { get; set; }
+
+        public string TestProp6 { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
